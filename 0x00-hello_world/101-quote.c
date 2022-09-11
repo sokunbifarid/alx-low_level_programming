89fcbb26f3b5ef0ce426cc66d5afd54e646b81f1
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-11 12:28:10 -0700

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry
+ * @void: null
+ * Return: function result
+ */
 int main(void) 
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(1);
+	return (1);
 }
