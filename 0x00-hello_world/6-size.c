e6128d25db16b42f9e83a193d23407e687341104
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-11 11:41:58 -0700

    push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@ long long int longerinteger;
 float floater;
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)",sizeof(character));
-	printf("Size of an int: %ld byte(s)",sizeof(integer));
-	printf("Size of a long int: %ld byte(s)",sizeof(longinteger));
-	printf("Size of a long long int: %ld byte(s)",sizeof(longerinteger));
-	printf("Size of a float: %ld byte(s)",sizeof(floater));
+	printf("Size of a char: %ld byte(s)\n",sizeof(character));
+	printf("Size of an int: %ld byte(s)\n",sizeof(integer));
+	printf("Size of a long int: %ld byte(s)\n",sizeof(longinteger));
+	printf("Size of a long long int: %ld byte(s)\n",sizeof(longerinteger));
+	printf("Size of a float: %ld byte(s)\n",sizeof(floater));
 	return(0);
 }
 
