17f65ff6c0452903908d1dee924f04f737f30ee9
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-11 12:16:31 -0700

    push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point
+ * @void: null value
+ * Return: outputs 0 because of int function
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
