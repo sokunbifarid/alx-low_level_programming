3a56e3e0b44a750848c8d166876306dcf8110b30
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-11 12:10:45 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- * Main function - C programming entry point
+ * main - C programming entry point
+ * @void: No parameter passed
+ *
+ * Return: returns 0 because function is of type int
  */
+
 int main(void)
 {
 	char value[100] = {"\"Programming is like building a multilingual puzzle"};
 	puts(value);
-
 	return (0);
+
 }
