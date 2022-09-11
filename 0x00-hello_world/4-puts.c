0ad7bfaebcecd03e1039e6f038e639ae50a10ffa
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-11 10:52:33 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-char c["Programming is like building a multilingual puzzle"];
+char c[50];
 int main(void)
 {
+	c = {"Programming is like building a multilingual puzzle"};
 	puts(c);
 	return(0);
 }
