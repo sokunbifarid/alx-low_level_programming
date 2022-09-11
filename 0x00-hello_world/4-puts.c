aa0f111e77896d3aa4c9df1bd1596438b75b3390
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-11 11:09:08 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* Entry Point */
 int main(void)
 {
-	char value[100] = {"Programming is like building a multilingual puzzle"};
+	char value[100] = {"\"Programming is like building a multilingual puzzle"};
 	puts(value);
 	return(0);
 }
