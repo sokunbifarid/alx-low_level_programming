27dd73350b5e34444aa6721a7f891672c6abfee0
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-11 12:22:58 -0700

    push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
  * main - entry point
  * @void: null
@@ -16,10 +14,15 @@ int main(void)
 	float floater;
 	
 	printf("Size of a char: %ld byte(s)\n",sizeof(character));
+
 	printf("Size of an int: %ld byte(s)\n",sizeof(integer));
+
 	printf("Size of a long int: %ld byte(s)\n",sizeof(longinteger));
+
 	printf("Size of a long long int: %ld byte(s)\n",sizeof(longerinteger));
+
 	printf("Size of a float: %ld byte(s)\n",sizeof(floater));
+
 	return(0);
 }
 
