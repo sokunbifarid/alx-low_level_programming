0293fed879e5c4fc34100003de566ddb79cdbd77
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 16:31:18 -0700

    push

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @void: null
+ * Return: value
+ */
+int main(void)
+{
+	char super;
+
+	for (super = '0'; super <= '9'; super++)
+	{
+		putchar(super);
+	}
+
+	for (super = 'a'; super <= 'f'; super++)
+	{
+		putchar(super);
+	}
+	return (0);
+
+}
