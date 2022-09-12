d7cbe2ad126f34efba82ddad3e8660edf4527f90
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 16:09:07 -0700

    push

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @void - empty
+ * Return: value
+ */
+int main(void)
+{
+	char cup;
+	
+	for (cup = '0'; cup <= '9'; cup++)
+	{
+		putchar(cup);
+	}
+	putchar('\n');
+	return (0);
+
+}
