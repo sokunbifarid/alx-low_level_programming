8d3aefb666bd00755b385e00c1dce7e7409e9585
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 15:54:58 -0700

    push

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - func
+ * @void: put
+ * Return: value
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d",i);
+	}
+	printf("\n");
+	return (0);
+}
