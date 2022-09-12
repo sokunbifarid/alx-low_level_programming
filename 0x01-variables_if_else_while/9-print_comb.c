a6f827c25b657a392930081c5f014e8b32552af5
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 16:40:15 -0700

    push

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,9 +10,12 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		putchar((char) i + '0');
-		putchar(',');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('$');
 	putchar('\n');
 	return (0);
 }
