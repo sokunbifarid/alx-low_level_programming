9eee51d0b25d6f046f1fc30c396103c887e83e3a
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 12:56:10 -0700

    push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,15 +19,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("is positive");
-	} 
+		printf("%d is positive",n);
+	}
 	else if (n == 0)
 	{
-		printf("is zero");
+		printf("%d is zero",n);
 	}
-       	else if (n < 0)
+	else if (n < 0)
 	{
-		printf("is negative");
+		printf("%d is negative",n);
 	}
 
 	return (0);
