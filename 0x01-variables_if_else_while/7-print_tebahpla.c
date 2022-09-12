92723139b66da155775b3a566d6c949c863e77c2
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 16:22:21 -0700

    push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @void: argue
+ * Return: result
+ */
+int main(void)
+{
+	char cot;
+	for (cot = 'z'; cot <= 'a'; cot--)
+	{
+		putchar(cot);
+	}	
+	putchar('\n');
+	return (0);
+}
