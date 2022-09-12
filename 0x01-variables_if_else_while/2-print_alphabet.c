db35f8cc10486a7c6acfb785b326cf27e5a60e20
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 15:36:42 -0700

    push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * @void: empty
+ * Return: int
+ */
+int main(void)
+{
+	int chops;
+
+	for (chops = 'a'; chops <= 'z'; chops++)
+	{
+		putchar(chops);
+	}
+}
