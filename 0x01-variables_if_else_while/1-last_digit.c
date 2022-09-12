131a870f6983d878f1b41e5902332601670d9b33
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 15:09:50 -0700

    push

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is less than 5 and not 0\n",n,n%10);
+		printf("Last digit of %d is %d and is less than 5\n",n,n%10);
 	}	
 
 	return (0);
