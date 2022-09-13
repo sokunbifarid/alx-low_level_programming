d20eefce73b8f68d1e39fe3c5ba2763980e49ace
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-12 17:09:04 -0700

    push

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	int i,j,k;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 2; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			for (k = 0; k <= 2; k++)
+			for (k = 0; k <= 9; k++)
 			{
 				if (i != j && j != k && k != i)
 				{
