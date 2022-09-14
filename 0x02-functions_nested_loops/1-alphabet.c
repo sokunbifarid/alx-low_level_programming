50f0a1da301772fd1507b043e14060709f3cd6f4
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-14 10:00:46 -0700

    push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,21 @@
-#include "1-main.h"
+#include "main.h"
 /**
  * main - func
  * @void: empty
  * Return: value
  */
+
+void print_alphabet(void)
+{
+	char cet;
+
+	for (cet = 'a'; cet <= 'z'; cet++)
+	{
+		putchar(cet);
+	}
+	putchar('\n');
+}
+
 int main(void)
 {
 	print_alphabet();
