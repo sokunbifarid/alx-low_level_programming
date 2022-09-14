3154d345d27d59beef7cd098c36a0862dec4ca5a
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-14 10:34:29 -0700

    push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,18 @@
-#include <stdio.h>
-
-
-void print_alphabet(void)
+#include <unistd.h>
+/**
+ *
+ *  * _putchar - writes the character c to stdout
+ *
+ *   * @c: The character to print
+ *
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *
+ *       */
+int _putchar(char c)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-	putchar(c);
+		return (write(1, &c, 1));
 }
