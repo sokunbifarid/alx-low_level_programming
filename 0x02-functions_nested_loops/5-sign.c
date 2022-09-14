4d8960bfc1a5068dbdd15d300db3801dd6570efc
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-14 12:31:06 -0700

    push

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,14 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		printf('-');
-		return (-1);
+		printf("-1");
 	}
 	else if (n > 0)
 	{
-		printf('+');
-		return (1);
+		printf("+1");
 	}
 	else
 	{
-		printf('0');
-		return (0);
+		printf("00");
 	}
 }
