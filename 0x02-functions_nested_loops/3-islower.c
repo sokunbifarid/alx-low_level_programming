b6cf5fbd056135bc035bee84c92306f9b28273f5
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-14 12:04:51 -0700

    push

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - func
+ * @c: value
+ * RETURN: value
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
