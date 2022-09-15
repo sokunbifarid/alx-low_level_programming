18926a189ce20190691b6c81eedec459c14e2905
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-14 21:52:30 -0700

    push

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 
 	int b;
 
-	for (a = 23; a > 0; a--)
+	for (a = 0; a < 23; a++)
 	{
-		for (b = 59; b > 0; b--)
+		for (b = 0; b < 59; b++)
 		{
 			if (b >= 10 && a >= 10)
 			{
