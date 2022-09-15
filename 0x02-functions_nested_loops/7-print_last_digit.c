21b14113c7f4ec75e99bc3ddd6dab65756c4a98a
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-14 22:33:53 -0700

    push

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
+#include <math.h>
 /**
  * print_last_digit - function
  * @b: value
  * Return: value
  */
-int print_last_digit(int b)
+void print_last_digit(int b)
 {
 	printf("%d%d", abs(b % 10), abs(b % 10));
 /*	return (b % 10);*/
-	return (0);
+//	return NAN;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int a);
-int print_last_digit(int b);
+void print_last_digit(int b);
 int add(int a, int b);
 void print_to_98(int n);
 void jack_bauer(void);
