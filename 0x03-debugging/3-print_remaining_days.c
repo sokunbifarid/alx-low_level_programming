46cd858ae30a4d06061d15997a2d1e3ccfcec5b2
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 12:43:08 -0700

    push

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,7 +14,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) || year % 100 == 0)
 	{
-		if (month >= 2 && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
@@ -33,8 +33,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-}
-int main(void)
-{
-	print_remaining_days(29,2,2000);
 }
