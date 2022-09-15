3e124151c8054a981210034732a21faf4605ca9a
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 11:48:43 -0700

    push

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *
  * * main - tests function that prints if integer is positive or negative
@@ -10,22 +8,13 @@
  *
  * */
 
-
-
 int main(void)
 
 {
-
 	int i;
 
-
-
 	i = 0;
-
 	positive_or_negative(i);
 
-
-
 	return (0);
-
 }
