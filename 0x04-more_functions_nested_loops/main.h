5115aa2f9b7bd9b403ad15c240d17719bb5b3ecd
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 19:57:58 -0700

    push

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,20 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <unistd.h>
-#include <stdio.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-
-	return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
