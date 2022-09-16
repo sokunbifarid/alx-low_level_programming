cf8c3e9ee2138f63ab74272298feaf1abd698fc9
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 19:24:44 -0700

    push

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h">
+/**
+ * _isdigit - function
+ * @c: value
+ * Return: function calue
+ */
+int _isdigit(int c);
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
