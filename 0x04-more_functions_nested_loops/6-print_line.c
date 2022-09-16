0b82f8be79c383504e71a17ee466fe0aeb008363
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 21:32:13 -0700

    push

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/* print_line - function
+ * @n: value
+ * Return: value
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	else
+	{
+		_putchar('\n');
+	}
+	}
+}
