07cfa49f1fe8f4eb397f14adab51af95a695de21
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 20:05:52 -0700

    push

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/** 
+ * print_numbers - function
+ * @void: null
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,5 +10,5 @@ int _putchar(char);
 int _isupper(int);
 int _isdigit(int);
 int mul(int, int);
-
+void print_numbers(void);
 #endif /* MAIN_H */
