759f165ffff96472d32fafae002c67e1cbe67706
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 20:46:29 -0700

    push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,9 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j <= 15; j++)
 		{
-			_putchar((char) j + '0');
+			_putchar(j + '0');
 		}
 	}
 	_putchar('\n');
