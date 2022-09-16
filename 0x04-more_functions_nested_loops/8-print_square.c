6b2e85290d92ab36f2b86c40df08e706dd555cac
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-16 11:17:58 -0700

    push

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - fucntion
+ * @size: value
+ */
+void print_square(int size)
+{
+	char s = '#';
+
+	int i;
+
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = 0; j <= size; j++)
+			{
+				_putchar(s);
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
