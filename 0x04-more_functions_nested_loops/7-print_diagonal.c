574514ff95490fa182b3372bed96d3a772463173
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 22:31:40 -0700

    push

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,12 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 0; j <= i; j++)
 			{
 				_putchar(' ');
 			}
-		_putchar('\\');
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
 	else
