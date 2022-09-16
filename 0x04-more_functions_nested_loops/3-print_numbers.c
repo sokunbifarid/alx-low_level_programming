48132bf2fc18c2c4c915399d35610ddf62c14c19
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-15 20:10:55 -0700

    push

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 /** 
  * print_numbers - function
  * @void: null
  */
-
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
 	}
