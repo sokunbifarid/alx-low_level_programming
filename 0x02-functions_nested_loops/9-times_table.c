fd2bec710657162b446b6a946fabdc13da9f4206
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-17 04:42:27 -0700

    push

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - function
+ * @void: null
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j + '0');
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ int print_last_digit(int b);
 int add(int a, int b);
 void print_to_98(int n);
 void jack_bauer(void);
+void times_table(void);
