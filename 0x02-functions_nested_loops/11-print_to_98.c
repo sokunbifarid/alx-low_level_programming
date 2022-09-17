db840c232dbca7382ceff1072e30df241d5cc55c
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-17 06:27:39 -0700

    push

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,10 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while(n <= 98)
+		while (n <= 98)
 		{
 			printf("%d", n++);
-			if (n != 98)
+			if (n != 99)
 			{
 				printf(",");
 				printf(" ");
@@ -20,10 +20,10 @@ void print_to_98(int n)
 	}
 	else if (n >= 99)
 	{
-		while(n >= 99)
+		while (n >= 99)
 		{
 			printf("%d", n--);
-			if (n != 98)
+			if (n != 99)
 			{
 				printf(",");
 				printf(" ");
