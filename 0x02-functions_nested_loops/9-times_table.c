1a3361f5dd5f73391500e8737c4fc9528fcc91c3
sokunbi farid <sokunbifarid1@gmail.com>
2022-09-17 04:46:53 -0700

    push

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,8 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar(i * j + '0');
+			_putchar(i * j / 10 + '0');
+			_putchar(i * j % 10 + '0');
 			if (j > 8)
 			{
 				continue;
