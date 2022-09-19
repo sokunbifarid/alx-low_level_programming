ac18615c34d63065e8ec8f247e0976aa2762ceed
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-19 18:17:36 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void swap_int(int *a, int *b);
-void swap_char(char *a, char *b);
+void flip_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
