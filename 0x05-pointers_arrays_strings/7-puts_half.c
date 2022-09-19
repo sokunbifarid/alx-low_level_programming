1eaa698151559d8e5d87b58fcb9a35fd96374b7f
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-19 18:50:54 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,14 +14,7 @@ void puts_half(char *str)
 
 	for (j = 0; j <= i; j++)
 	{
-		if (j == 0)
-		{
-			_putchar(str[j]);
-		}
-		else if (j % 2 == 0)
-		{
-			_putchar(str[j]);
-		}
+		_putchar(str[j]);
 	}
 	_putchar('\n');
 }
