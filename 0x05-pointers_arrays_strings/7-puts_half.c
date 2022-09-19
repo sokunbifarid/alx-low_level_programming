3ee0fd4c9d414dac83d663d7451f0678e04516a8
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-19 18:50:19 +0100

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* puts_half - function
+* @str: string
+*/
+
+void puts_half(char *str)
+{
+	int i = (_strlen(str) - 1) / 2;
+
+	int j;
+
+	for (j = 0; j <= i; j++)
+	{
+		if (j == 0)
+		{
+			_putchar(str[j]);
+		}
+		else if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
+}
+
+/**
+ * _strlen - function
+ * @s: value
+ * Return: value
+ */
+
+int _strlen(char *s)
+{
+	int i = 0, j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i++;
+	}
+	return (i);
+}
