19cdf22fe1a2ec2edb058d27796a24b737f06fb3
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-19 18:42:34 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,5 +8,31 @@
 
 void puts2(char *str)
 {
-	puts(str);
+	int i = _strlen(str) - 1;
+
+	if (i == 0)
+	{
+		puts(str[i]);
+	}
+	else if (i % 2 == 0)
+	{
+		puts(str[i]);
+	}
+}
+
+/**
+ * _strlen - function
+ * @s: value
+ * Return: value
+ */
+
+int _strlen(char *s)
+{
+	int i = 0, j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i++;
+	}
+	return (i);
 }
