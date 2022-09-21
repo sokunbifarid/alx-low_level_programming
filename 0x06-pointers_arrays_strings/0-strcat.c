861f862e08bb0723136da289d366d77445b519c4
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-21 16:20:07 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char i = "";
-	
-	char space = " ";
-	
-	strcat(i,dest);
-	strcat(i,space);
-	strcat(i,src);
-	return (i);
-}
+	int j = 0;
+	while (dest[j] != '\0')
+	{
+		_putchar(dest[j] + '0');
+		j++;
+	}
+	_putchar(' ');
+	if (dest[j] == '\0')
+	{
+		j = 0;
+	}
+	while (src[j] != '\0')
+	{
+		_putchar(src[j] + '0');
+		j++;
+	}
+	_putchar('\n');
