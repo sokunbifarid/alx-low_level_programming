fd78445487fbd0ee2712c6f06d1534c2f0e614f0
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-21 15:54:19 +0100

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strcat - function
+* @dest: value
+* @src: value
+* Return: value
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char i = "temp";
+	
+	i = dest + src;
+	return (i)
+}
