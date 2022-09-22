54953b4ee4c0f441c2a7f756b50beb486a572e25
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-22 05:17:50 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strncat - function
+* @dest: value
+* @src: value
+* @n: value
+* Return: value
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int j = 0, i = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	for (i = 0; i <= n, i++)
+	{
+		j++;
+		dest[j] = src[i];
+	}
+	return (dest);
+}
