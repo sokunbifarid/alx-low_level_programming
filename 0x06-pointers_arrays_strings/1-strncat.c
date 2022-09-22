32f8b50e3b29a41aae077f5c2f7b3cf89aa23533
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-22 05:29:03 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int j = 0, i = 0;
+	
 	while (dest[j] != '\0')
 	{
 		j++;
 	}
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[j] = src[i];
 		j++;
