e97b3dc1adacb3a7bcea4aa4488bbc3a0305fa9b
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-23 05:04:33 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reverse_array - function
+ *@a: value
+ *@n: value
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0; j = n;
+
+	int map[5000] = {0,1,2,3};
+
+	for (j = n; j >= 0; j--)
+	{
+		a[j] = map[i];
+		i++
+	}
+}
