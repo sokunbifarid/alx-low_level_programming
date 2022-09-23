5a33c42e337e8a4ab6506275825b1de9710307ff
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-23 06:27:29 +0100

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *leet - function
+ *@sat: value
+ *Return: value
+ */
+
+char *leet(char *sat)
+{
+	int i = 0, j = 0, cap_value = 7;
+
+	char buggy[5000] = {'H','E','L','I','O','!','!','!'};
+
+	while (sat[++i]))
+	{
+		while (j <= cap_value)
+		{
+			if (sat[i] == buggy[j])
+			{
+				sat[i] = j + '0';
+			}
+			if (sat[i] - 32 == buggy[j])
+			{
+				sat[i] = j + '0';
+			}
+		}
+	}
+	return (sat);
+}
