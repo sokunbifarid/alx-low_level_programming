39c3501a422264b1889132fa4d5abb5862f5ecda
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-23 05:37:30 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *string_toupper - function
+ *@cara: value
+ *Return: value
+ */
+
+char *string_toupper(char *cara)
+{
+	int i = 0;
+	
+	while(cara[i] != '\0')
+	{
+		if (cara[i] >= 'a' && cara[i] <= 'z')
+		{
+			cara[i] -= 32;
+		}
+		i++;
+	}
+}
