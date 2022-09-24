3034205a671487e8c09e3208885514da25deff00
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-24 03:52:22 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,17 +8,14 @@
 
 void reverse_array(int *a, int n)
 {
-	int i = 0;
+	int i, j = n - 1;
 
-	int j = n - 1;
+	int m[] = {1,2,3,4,5};
 
-	int map[] = {0,1,2,3};
-
-	for (; j > n / 2; j--)
+	for (i = 0; i < n; i++)
 	{
-		i = a[n - 1 - j];
-		a[n - 1 - j] = a[j];
-		i++;
-		a[j] = i;
+		m[i] = a[j];
+		j--;
 	}
+	a = m;
 }
