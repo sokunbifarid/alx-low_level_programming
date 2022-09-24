257162c14d74484070a7148194471b6d74785ca5
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-24 05:16:05 +0100

    Create 103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *infinite_add - function
+ *@n1: value
+ *@n2: value
+ *@r: value
+ *@size_r: value
+ *Return: value
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r)
+}
