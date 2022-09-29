21e034ad6bb8115114b89b5843044de3094797aa
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-29 18:21:02 +0100

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *print_diagsums
+ *@a: value
+ *@size: value
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	while (i < size)
+	{
+		j += a[i];
+		a += size;
+		i++;
+	}
+
+	a -= size;
+
+	while (i < size)
+	{
+		k += a[i];
+		a -= size;
+		i++;
+	}
+
+	printf("%d, %d\n", j, k);
+}
