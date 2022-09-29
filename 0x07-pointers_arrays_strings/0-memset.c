cd4a83842336b44ea88bf36a210c402b05b587d1
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-29 17:50:13 +0100

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memset - function
+ *@s: value
+ *@b: value
+ *@n: value
+ *Return: value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	
+	for (i = n; i >= 0; i--)
+	{
+		s[n - 1] = b;
+	}
+	return (s);
+}
