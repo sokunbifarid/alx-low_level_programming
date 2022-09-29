db1c91841781188c0a54b374672d90fa65fe2661
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-09-29 18:00:29 +0100

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strchr - function
+ *@s: value
+ *@c: value
+ *Return: value
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != c && s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
