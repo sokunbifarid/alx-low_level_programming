8e8c7ca4ee17366b9c2532fce26d928237147e92
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-28 22:14:06 +0100

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - square root function
+ *@n: value
+ * Return: Value
+ */
+
+int _sqrt_recursion(int n)
+{
+	int a = 0;
+
+	if (n >= 0 && n < 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (root(0, n));		
+	}
+}
+
+int root(int a, int n)
+{
+	if (a > n / 2)
+	{
+		return (-1);
+	}
+	else if ( a * a == n)
+	{
+		return (a);
+	}
+	return (root(a + 1, n));
+}
