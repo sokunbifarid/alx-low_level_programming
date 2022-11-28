868f57e311c49f549143776f034a6eb9f3cb969b
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-28 22:57:45 +0100

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime_number - function to return prime numbers
+ * @n: value
+ * Return: Value
+ */
+
+int is_prime_number(int n)
+{
+	int f = n / 2;
+
+	if (n > 1)
+	{
+		return (check_prime(n, f));
+	}
+}
+
+int check_prime(int i, int j)
+{
+	if (j <= 1)
+	{
+		return (1);
+	}
+	else if (i % j == 0)
+	{
+		return (0);
+	}
+	return (check_prime(i, j - 1));
+}
