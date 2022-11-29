1190ceb48b71e84d5595902a38360f1d067b72f4
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-29 18:34:19 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* main - prints multiplication
-*of two numbers
-*@argc: number of arguments
-*@argv: array of arguments
-*Return: returns 0
+* main - autoloaded function
+*@argc: argument 1
+*@argv: argument 2
+*Return: return value
 */
 
 int main(int argc, char *argv[])
 {
-	int i, j, mul;
+	int f1, f2, f0;
 
-	if (argc <= 2)
+	if (argc > 2)
+	{
+		f1 = atoi(argv[1]);
+		f2 = atoi(argv[2]);
+		f0 = f1 * f2;
+
+		printf("%d\n", f0);
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-	mul = i * j;
-
-	printf("%d\n", mul);
-	return (0);
 }
