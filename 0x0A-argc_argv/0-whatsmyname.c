16e6c6ab926effb7760876843ede5d014a1c391d
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-29 18:27:43 +0100

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - autoload function
+*@argc: argument 1
+*@argv: argument 2
+*Return: return value
+*/
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
