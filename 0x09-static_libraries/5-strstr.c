2693b02836f6e2c0f8291dc01cb0493e03a69d75
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-30 06:19:05 +0100

    push

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
-*_strstr - The _strstr() function finds the first occurrence
-* of the substring needle in the string haystack.
-* The terminating null bytes (\0) are not compared
-*@haystack: string where the search is made
-*@needle: string whose occurence is searched in haystack
-*Return:Returns a pointer to the beginning of the located
-* substring, or NULL if the substring is not found.
-*/
+ * _strstr - function
+ * @haystack: character
+ * @needle: character
+ * Return: return value
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i;
+	int i = 0;
 
 	if (*needle == 0)
+	{
 		return (haystack);
+	}
 
 	while (*haystack)
 	{
@@ -24,11 +24,16 @@ char *_strstr(char *haystack, char *needle)
 		{
 			do {
 				if (needle[i + 1] == '\0')
+				{
 					return (haystack);
+				}
 				i++;
 			} while (haystack[i] == needle[i]);
 		}
-		haystack++;
+		else
+		{
+			haystack++;
+		}
 	}
 	return ('\0');
 }
