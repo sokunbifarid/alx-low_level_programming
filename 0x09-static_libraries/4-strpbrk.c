44ce1dcf79fd534eaea9a8bdc211b0e829a1c55c
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-30 06:17:01 +0100

    push

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,15 +1,11 @@
 #include "main.h"
+
 /**
-*_strpbrk - The _strpbrk() function locates the first
-* occurrence in the string s of any of the bytes in
-*the string accept
-*
-*@s: string where search is made
-*@accept: string where searched bytes are located
-*
-*Return:Returns a pointer to the byte in s that matches
-* one of the bytes in accept, or NULL if no such byte is found
-*/
+ *_strpbrk - function
+ * @s: character
+ * @accept: character
+ * Return: return value
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
@@ -20,10 +16,11 @@ char *_strpbrk(char *s, char *accept)
 		for (i = 0; accept[i]; i++)
 		{
 			if (accept[i] == *s)
+			{
 				return (s);
+			}
 		}
 		s++;
 	}
-
 	return ('\0');
 }
