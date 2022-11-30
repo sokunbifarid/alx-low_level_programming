328ce8d1d3386a6e96d1c4c59b5668f806e9f838
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-30 06:22:19 +0100

    push

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
-* _strcpy - Copy paste string
-*@dest: destination
-*@src: source
-*Return: dest
-*/
+ * _strcpy - function
+ * @dest: character
+ * @src: character
+ * Return: return value
+ */
+
 char *_strcpy(char *dest, char *src)
 {
-int inc = 0;
-while (*(src + inc) != '\0')
-{
-*(dest + inc) = *(src + inc);
-inc++;
-}
-*(dest + inc) = '\0';
-return (dest);
+	int i = 0;
+	
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
 }
