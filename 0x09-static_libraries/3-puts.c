86a9d55354851728a8e700dd54bbeeec12b9f909
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-30 06:12:00 +0100

    push

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,15 @@
 #include "main.h"
+
 /**
- *_puts - prints a string
- * @str: string to print
- *
- * Description: prints a string
- * On success: return the number of characters printed
+ *_puts - function
+ * @str: character
  */
 
 void _puts(char *str)
 {
-while (*str)
-_putchar(*str++);
-
-_putchar('\n');
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
