ee244e848046ede9ffbdd30369c3e12ea8a14640
Sokunbi Farid <87651046+sokunbifarid@users.noreply.github.com>
2022-11-30 06:15:20 +0100

    push

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,13 +1,19 @@
 #include "main.h"
+
 /**
-*_isalpha - controls if a character is alphabetical
-*@c: character to be verified
-*Return: return 0 or 1
+ * _isalpha - function
+ * @c: character
+ * Return: return value
 */
+
 int _isalpha(int c)
 {
-if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-return (1);
-
-return (0);
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
